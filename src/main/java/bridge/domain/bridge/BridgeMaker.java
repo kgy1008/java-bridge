052f7aa5f79da11db3e4f318f7de9ffca6ad96d9
kgy1008 <gayeon1008@g.skku.edu>
2024-12-03 15:25:02 +0900

    feat: 다리 생성

---
+++ src/main/java/bridge/domain/bridge/BridgeMaker.java
@@ -0,0 +1,28 @@
+package bridge.domain.bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeMaker {
+
+    private static final int MOVABLE = 1;
+
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int num = bridgeNumberGenerator.generate();
+            if (num == MOVABLE) {
+                bridge.add("U");
+            } else {
+                bridge.add("D");
+            }
+        }
+        return bridge;
+    }
+}
