77caee0afbc0e1bd08992ae8c817d73a4094b01c
kgy1008 <gayeon1008@g.skku.edu>
2024-12-03 15:47:56 +0900

    feat: 에러 메세지 추가

--- src/main/java/bridge/common/ErrorMessage.java
+++ src/main/java/bridge/common/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     INVALID_INPUT_TYPE("숫자로 입력해주세요."),
     INVALID_INPUT_RANGE("다리 길이는 최소 3, 최대 20입니다."),
-    INVALID_RETRY_INPUT("재시도는 R 종료는 Q를 입력해주세요.");
+    INVALID_RETRY_INPUT("재시도는 R 종료는 Q를 입력해주세요."),
+    INVALID_USER_INPUT("U 또는 D를 입력해주세요.");
 
     private static final String HEADER = "[ERROR ]";
     private final String message;
